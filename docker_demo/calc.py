3ef6788bf166ae5a1d3114753a89e9bd214fb70f
KostasPanteldis <kpantelidis91@gmail.com>
2026-06-02 15:21:49 +0100

    docker_update

---
+++ docker_demo/calc.py
@@ -0,0 +1,25 @@
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def get_sum(self):
+        return self.num1 + self.num2
+
+    def get_diff(self):
+        return self.num1 - self.num2
+
+    def get_product(self):
+        return self.num1 * self.num2
+    
+    def get_quotient(self):
+        # Fix: Check for division by zero
+        if self.num2 == 0:
+            return "Error: Can't devide by 0"
+        return self.num1 / self.num2
+    
+
+    
+myCalc = Calculator(144,12)
+print("The result of 144 devided by 12 equals to ", myCalc.get_quotient())
+
